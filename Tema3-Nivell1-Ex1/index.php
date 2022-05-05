cc070b5552cbeaef1aab2b94077c1e7771d08fe0
laqmarc <laqmarc@gmail.com>
2022-05-05 12:03:19 +0200

    Commit T3 N1 Ex1

---
+++ Tema3-Nivell1-Ex1/index.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tema 3 Nivell 1 Ex 1</title>
+</head>
+<body>
+    
+<?php
+   /*
+Defineix una variable de cada tipus: integer,double,string i boolean.
+ Després, imprimeix-les per pantalla.
+*/
+
+$integer = 5;
+$double = 500.5;
+$string = "Hola soc en Marc ";
+$boolean = true;
+
+echo $string, " he quedat a les $integer per cobrar $double i espero que sigui  $boolean";
+
+?>
+</body>
+</html>
+
+
+
+
+
