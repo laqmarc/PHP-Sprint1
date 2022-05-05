f32a496d7e090b39c516ebfcab2745a828cd91b1
laqmarc <laqmarc@gmail.com>
2022-05-05 13:24:54 +0200

    T3-N1-Ex3

---
+++ Tema3-Nivell1-Ex3/index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tema 3 Nivell 1 Ex 3</title>
+</head>
+<body>
+    <style>
+        h2{font-size:4rem;font-weight:800;}
+    </style>
+<?php
+echo "<h1>Tema 3 Nivell 1 Ex 3</h1>";
+/*
+
+-- Exercici 3
+Crea una constant que contingui el teu nom i el imprimeixi per pantalla.
+ Has imprimir-la amb format de títol i lletra en negreta en HTML.
+*/
+
+define("nom", "Marc Llobera");
+echo "<h2>" . nom . "</h2>";
+
+
+?>
+</body>
+</html>
