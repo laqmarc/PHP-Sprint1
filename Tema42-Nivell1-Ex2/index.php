3301bb6fb057157fc1a35088555a5ca023031a8a
laqmarc <laqmarc@gmail.com>
2022-05-16 12:24:59 +0200

    Update index.php

--- Tema42-Nivell1-Ex2/index.php
+++ Tema42-Nivell1-Ex2/index.php
@@ -54,6 +54,17 @@
    </form>
 
 
+<!-- 
+
+Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. 
+
+Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la forma area().
+
+A l'arxiu main defineix dos objectes, un triangle i un rectangle i truca al mètode area de cadascun.
+
+ -->
+
+
 
 <?php
 
@@ -66,11 +77,7 @@ abstract class Shape{
         $this->width = $width;
         $this->height = $height;
     }
-
-    public function area(){
-        return $this->width * $this->height ;
-    }
-
+    
 }
 
 class Triangle extends Shape{
